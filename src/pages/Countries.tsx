9c3bab4fab7e97c628a15effea4fbcfcef8405d0
Giorgi <giorgigrigolishvili@gmail.com>
2025-08-12 21:58:40 +0400

    basic home page, plus styling

--- src/pages/Countries.tsx
+++ src/pages/Countries.tsx
@@ -33,9 +33,34 @@ export default function Country() {
         setSearchInput={setSearchInput}
         searchInput={searchInput}
       />
-      {filteredCoutries.map((country) => (
-        <p>{country.name}</p>
-      ))}
+      <div className="px-[5.5rem]">
+        {filteredCoutries.map((country) => (
+          <div
+            className="mt-[3.2rem]
+          bg-white shadow-[0_7px_2px_0_rgba(0,0,0,0.03)]
+          rounded-[0.5rem]"
+          >
+            <div>
+              <img src={country.flags.svg} alt="country-img" />
+              <div
+                className="mt-[2.4rem] pl-[2.4rem]
+                pb-[4.6rem]"
+              >
+                <h1>{country.name}</h1>
+                <p>
+                  Population:<span> {country.population}</span>
+                </p>
+                <p>
+                  Region:<span> {country.region}</span>
+                </p>
+                <p>
+                  Capital:<span> {country.capital}</span>
+                </p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- type.d.ts
+++ type.d.ts
@@ -1,4 +1,9 @@
 export type TCountry = {
   name: string;
   region: string;
+  flags: {
+    svg: string;
+  };
+  population: string;
+  capital: string;
 };
