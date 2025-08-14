d5aec8186f83aaba7401623ffa52957100694463
Giorgi <giorgigrigolishvili@gmail.com>
2025-08-15 01:24:31 +0400

    wip:dark mode

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,10 @@
+import { useState } from "react";
+
 export default function Header() {
+  const [dark, setDark] = useState<boolean>(false);
+  const handleClick = () => {
+    document.documentElement.classList.toggle("dark");
+  };
   return (
     <div
       className="bg-white shadow-[0_2px_4px_0_rgba(0,0,0,0.06)]
@@ -19,22 +25,43 @@ export default function Header() {
         <div
           className="flex items-center gap-[0.8rem]
           cursor-[pointer]"
+          onClick={() => {
+            handleClick();
+            setDark((prev) => !prev);
+          }}
         >
-          <svg
-            width="16"
-            height="16"
-            viewBox="0 0 16 16"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              clipRule="evenodd"
-              d="M10.8426 11.052C7.73486 11.052 5.21543 8.74226 5.21543 5.89457C5.21543 4.82024 5.57343 3.82526 6.18514 3C3.75229 3.75612 2 5.86498 2 8.35045C2 11.4708 4.75943 14 8.16286 14C10.8743 14 13.1757 12.3945 14 10.1636C13.1 10.7238 12.0129 11.052 10.8426 11.052Z"
-              fill="white"
-              stroke="#111517"
-            />
-          </svg>
+          {!dark ? (
+            <svg
+              width="16"
+              height="16"
+              viewBox="0 0 16 16"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M10.8426 11.052C7.73486 11.052 5.21543 8.74226 5.21543 5.89457C5.21543 4.82024 5.57343 3.82526 6.18514 3C3.75229 3.75612 2 5.86498 2 8.35045C2 11.4708 4.75943 14 8.16286 14C10.8743 14 13.1757 12.3945 14 10.1636C13.1 10.7238 12.0129 11.052 10.8426 11.052Z"
+                fill="white"
+                stroke="#111517"
+              />
+            </svg>
+          ) : (
+            <svg
+              width="16"
+              height="16"
+              viewBox="0 0 16 16"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M10.8426 11.052C7.73486 11.052 5.21543 8.74226 5.21543 5.89457C5.21543 4.82024 5.57343 3.82526 6.18514 3C3.75229 3.75612 2 5.86498 2 8.35045C2 11.4708 4.75943 14 8.16286 14C10.8743 14 13.1757 12.3945 14 10.1636C13.1 10.7238 12.0129 11.052 10.8426 11.052Z"
+                fill="white"
+              />
+            </svg>
+          )}
           <p
             className="text-[1.2rem] font-[600]
             xl:text-[1.6rem]"
